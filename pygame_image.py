d9322f7d43bc371cfcd7079fbedf079aebf51ba8
c0a2418041 <c0a2418041@edu.teu.ac.jp>
2025-06-10 16:39:09 +0900

    演習１：左に流れるこうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -21,14 +21,16 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rect.move_ip(0,-1)
-        if key_lst[pg.K_DOWN]:
-            kk_rect.move_ip(0,1)
-        if key_lst[pg.K_LEFT]:
-            kk_rect.move_ip(-1,0)
+        #if key_lst[pg.K_UP]:
+         #   kk_rect.move_ip(0,-1)
+        #if key_lst[pg.K_DOWN]:
+         #   kk_rect.move_ip(0,1)
+        #if key_lst[pg.K_LEFT]:
+          #  kk_rect.move_ip(-1,0)
         if key_lst[pg.K_RIGHT]:
             kk_rect.move_ip(1,0)
+        else:
+            kk_rect.move_ip(-1,0)
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
